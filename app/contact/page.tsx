0b9249d687b71b8c400a2e450a2ac018e9c72701
Aman Roy <royaman56456@gmail.com>
2025-07-04 22:29:44 +0530

    Committing NextJS Code

---
+++ app/contact/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default async function Contact() {
+    console.log("Hey is this in the server or client?");
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    console.log(posts);
+    
+    
+    
+  return (
+    <div>
+      Contact Page
+    </div>
+  )
+}
